0e911518111002ece6f479a402c953277b553136
INTERNAL\tfischle <tfischle@idmod.org>
2021-02-07 23:07:43 -0800

    add 2D x,y index

--- grid_block_idx.py
+++ grid_block_idx.py
@@ -12,18 +12,12 @@ mod = SourceModule("""
 
 #include <cstdint>
  
-__global__ void update(
-    unsigned int count)
+__global__ void update()
 {   
     // const int idx = threadIdx.x + blockIdx.x * blockDim.x;   // 1D
     
     const int idx_x = blockIdx.x * blockDim.x + threadIdx.x;    // 2D index for x-coord
     const int idx_y = blockIdx.y * blockDim.y + threadIdx.y;    // 2D index for y-coord
-    
-    const int blockId = blockIdx.x + blockIdx.y * blockDim.x;   // 2D
-    const int idx = blockId * (blockDim.x * blockDim.y) + (threadIdx.y * blockDim.x) + threadIdx.x; // global index   
-    
-    if(idx >= count) return;
     
     printf("%10d  %10d  %10d  %10d %10d  %10d\\n", 
            threadIdx.x, threadIdx.y, blockIdx.x, blockIdx.y, blockDim.x, blockDim.y);
@@ -32,8 +26,6 @@ __global__ void update(
 }
 """)
 
-last_id = 31
-
 if __name__ == '__main__':
     pycuda.driver.init()
 
@@ -41,8 +33,8 @@ if __name__ == '__main__':
 
     print("threadIdx.x | threadIdx.y | blockIdx.x | blockIdx.y | blockDim.x | blockDim.y ")
 
-    print_index(numpy.uint32(last_id),
-                block=(3, 2, 1),  # max 36 lines
+    print_index(
+                block=(5, 3, 1),  # max 36 lines
                 grid=(3, 2, 1),
                 time_kernel=True
                 )
