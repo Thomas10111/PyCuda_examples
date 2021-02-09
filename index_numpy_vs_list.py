a52cfac6df18d6d67fdc329ff43b60923a833a46
INTERNAL\tfischle <tfischle@idmod.org>
2021-02-09 11:01:11 -0800

    first commit

--- index_numpy_vs_list.py
+++ index_numpy_vs_list.py
@@ -4,7 +4,14 @@ If possible do not use numpy arrays in a loop.
 """
 
 from timeit import default_timer
+from datetime import datetime
 import numpy
+
+COUNT = 10_000_000
+
+def arrayOfFloat32(count):
+    return numpy.zeros(count, dtype=numpy.float32)
+
 
 duration = 1*365
 NumberOfIndividuals = 1000
@@ -44,10 +51,31 @@ start = default_timer()
 my_numpy_32 += dt
 print("duration array add float32: ", default_timer() - start)
 
-# Output:
-# duration:  0.0469182
-# duration:  0.1338455
-# duration mixing 32 and default type:  0.8713339
-# duration casting to float32:  0.2828889000000001
-# duration array add default:  0.0003012999999998378
-# duration array add float32:  0.00014270000000005112
+start = datetime.now()
+data = arrayOfFloat32(COUNT)
+data += numpy.float32(1)
+finish = datetime.now()
+print(f"{finish - start} to increment {COUNT} float32 in array.")
+
+start = datetime.now()
+data = arrayOfFloat32(COUNT)
+data += 1.0
+finish = datetime.now()
+print(f"{finish - start} to increment {COUNT} float32 in array.")
+
+start = datetime.now()
+data = arrayOfFloat32(COUNT)
+data += dt
+finish = datetime.now()
+print(f"{finish - start} to increment {COUNT} float32 in array.")
+
+
+# duration:  0.0544421
+# duration:  0.1371477
+# duration mixing 32 and default type:  0.8986641
+# duration casting to float32:  0.28694280000000005
+# duration array add default:  0.00029930000000000234
+# duration array add float32:  0.00015209999999998836
+# 0:00:00.021934 to increment 10000000 float32 in array.
+# 0:00:00.026749 to increment 10000000 float32 in array.
+# 0:00:00.022172 to increment 10000000 float32 in array.
